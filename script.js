09df16a865b6dd2db24bdab3b027871071e6d935
ilhye <144883820+ilhye@users.noreply.github.com>
2024-05-22 18:01:03 +0800

    update html

---
+++ script.js
@@ -0,0 +1,9 @@
+const weather = document.getElementById('img-weather');
+
+function weatherVisual() {
+    var dict = {
+        FirstName: "Chris",
+        "one": 1,
+        1: "some value"
+      };
+}
