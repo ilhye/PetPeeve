1f17618eb4093aa318b34012e7a8cbd1b5b6f5c7
ilhye <144883820+ilhye@users.noreply.github.com>
2024-06-29 17:43:57 +0800

    finish frontend

--- script.js
+++ script.js
@@ -1,9 +1,76 @@
-const weather = document.getElementById('img-weather');
+const weather = document.getElementById("img-weather");
+const messageList = document.getElementById("message");
+const temperature = document.getElementById("temperature");
+const tempMode = document.getElementById("temp-mode");
+const date = document.getElementById("date");
+const cardColor = document.getElementById("weather-card");
+const headerBg = document.getElementById("proj-header");
+
+function getRandomWeather() {
+  const weathers = ["cloudy", "sunny"];
+  const randomIndex = Math.floor(Math.random() * weathers.length);
+  return weathers[randomIndex];
+}
+
+function formatDate() {
+  const day = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  const monthNows = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+
+  const dateNow = new Date();
+  const dayOfWeek = day[dateNow.getDay()];
+  const dateOfWeek = String(dateNow.getDate());
+  const monthNow = monthNows[dateNow.getmonthNow()];
+
+  date.innerHTML = `${dayOfWeek}, ${dateOfWeek} ${monthNow}`;
+}
+
+function temp() {
+  // insert logic for changing temperature and its description
+  temperature.innerHTML = "35°C";
+  tempMode.innerHTML = "Sunny";
+}
 
 function weatherVisual() {
-    var dict = {
-        FirstName: "Chris",
-        "one": 1,
-        1: "some value"
-      };
+  const weatherCondition = getRandomWeather();
+
+  switch (weatherCondition) {
+    case "cloudy":
+      weather.src = "./assets/cloudy.png";
+      messageList.innerHTML = "";
+      messageList.innerHTML = `
+        <li>Keep your pet comfortable indoors with cozy bedding and toys.</li>
+        <li>Take short walks with your pet and avoid prolonged exposure to damp weather.</li>
+        <li>Monitor your pet's behavior for signs of anxiety during cloudy periods.</li>`;
+      headerBg.style.backgroundColor = "#d6ffff";
+      cardColor.style.background =
+        "linear-gradient(121deg, #f6f6f6 31.21%, #96f1ff 167.04%)";
+      break;
+    case "sunny":
+      weather.src = "./assets/sunny.png";
+      messageList.innerHTML = "";
+      messageList.innerHTML = `
+        <li>Hydrate your pet with fresh water, limit midday walks, and provide shade for cool naps.</li>
+        <li>Cool down your pet with damp towels, early walks, and kiddie pools for a refreshing dip.</li>
+        <li>Fresh water, shady spots, and paw protection are your pet's summer essentials!</li>`;
+      headerBg.style.backgroundColor = '#fcffd6';
+      cardColor.style.background =
+        "linear-gradient(121deg, #f6f6f6 31.21%, #fcc949 167.04%)";
+
+      break;
+  }
 }
+
+weatherVisual();
